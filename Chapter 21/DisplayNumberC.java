e73cef029689efa7f996a40c927a2d6495e06ed6
Timongold <destinyagi9@gmail.com>
2026-03-02 13:56:01 +0100

    Remove Chapter 21 folder

--- Chapter 21/DisplayNumberC.java
+++
@@ -1,8 +0,0 @@
-public class DisplayNumberC{
-	public static void main(String[] args){
-		System.out.print("1 ");
-        System.out.print("2 ");
-        System.out.print("3 ");
-        System.out.print("4");
-	}
-}
