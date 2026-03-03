99affd3ea386fc7f40b901687f16cbcf684f88fa
Timongold <destinyagi9@gmail.com>
2026-03-03 10:14:26 +0100

    Cleaned chapter 2 folder structure

--- Chapter 2/Chapter 3/Exercise3_15D.java
+++
@@ -1,9 +0,0 @@
-public class Exercise3_15D{
-	public static void main(String[] args_){
-		int y = 5;  // starting value
-		while (y > 0) {
-			System.out.println(y);
-			--y; // decrement so the loop eventually ends
-		}
-	}
-}
